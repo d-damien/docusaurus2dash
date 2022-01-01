ec6bec329674a5ce4a0a8e82c75d4191a6cc6e42
d-damien <d-damien@users.noreply.github.com>
2022-01-01 19:37:40 +0100

    slug -> name

--- docset.generate.sh
+++ docset.generate.sh
@@ -3,7 +3,6 @@
 # WIP
 
 # Edit variables
-SLUG='dayjs'
 NAME='DayJS'
 REPO='https://github.com/dayjs/dayjs-website'
 FOLDER='dayjs-website'
@@ -14,13 +13,13 @@ for dep in ${dependencies[@]}; do
 done
 
 # cleanup
-rm -rf $SLUG.docset /tmp/$SLUG
+rm -rf $NAME.docset /tmp/$NAME
 
 # basic folders & files
 [ -d $FOLDER ] || git clone $REPO
-mkdir -p $SLUG.docset/Contents/Resources/Documents/
-mkdir /tmp/$SLUG/
-sqlite3 $SLUG.docset/Contents/Resources/docSet.dsidx '
+mkdir -p $NAME.docset/Contents/Resources/Documents/
+mkdir /tmp/$NAME/
+sqlite3 $NAME.docset/Contents/Resources/docSet.dsidx '
   CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);
   CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);
 '
@@ -33,18 +32,18 @@ categories=$(jq -r '.docs | keys_unsorted | .[]' < $FOLDER/website/sidebars.json
 # concatenate each category in single file + index.md
 for cat in ${categories[@]}; do
   for subcat in $(jq -r ".docs[\"$cat\"] | .[]" < $FOLDER/website/sidebars.json); do
-    cat $FOLDER/docs/$subcat.md >> /tmp/$SLUG/$cat.md
+    cat $FOLDER/docs/$subcat.md >> /tmp/$NAME/$cat.md
   done
 
   # replace docusaurus ---\n slug \n title \n--- with pandoc markdown title
-  perl -0777 -pi -e 's/---\n[^:]*: ([^\n]*)\n[^:]*: ([^\n]*)\n---/## \2 {#\1}/g' /tmp/$SLUG/$cat.md
+  perl -0777 -pi -e 's/---\n[^:]*: ([^\n]*)\n[^:]*: ([^\n]*)\n---/\n\n## \2 {#\1}/g' /tmp/$NAME/$cat.md
   # md -> html
   pandoc -f markdown -t html -s\
     --metadata pagetitle="$cat"\
-    -o $SLUG.docset/Contents/Resources/Documents/$cat.html\
-    /tmp/$SLUG/$cat.md
+    -o $NAME.docset/Contents/Resources/Documents/$cat.html\
+    /tmp/$NAME/$cat.md
   # sqlite
-  sqlite3 $SLUG.docset/Contents/Resources/docSet.dsidx "
+  sqlite3 $NAME.docset/Contents/Resources/docSet.dsidx "
     INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES ($cat, 'Category', $cat.html);
   "
 done
